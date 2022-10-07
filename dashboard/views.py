af23f1e987b7df9312240feaeba467f9f16ca7e5
y-iwata-estyle <y.iwata@estyle-inc.jp>
2022-10-07 15:10:15 +0900

    edited sidebar

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import matplotlib
+import matplotlib.pyplot as plt
+
+
     
 class MainView:
    def dashboard_view(request): # ViewFunctionと呼ぶ
@@ -8,3 +12,4 @@ class MainView:
        chart_data = [[1, 5, 1, 0, 0, 0],[1, 5, 1, 0, 0, 0]]
        context = {'chart_data': chart_data, 'chart_label': chart_label}
        return render(request,template_name,context)
+
